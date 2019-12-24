e711359480f54e24c13e940cb7cb0271ece36573
Metaxol <45857811+Metaxol@users.noreply.github.com>
2019-12-24 17:15:59 +0100

    Utility Class
    - finished string-line-splicing method
    - started text-scrolling method

---
+++ ComfyPixel/Assets/Utility.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Utility : MonoBehaviour {
+
+    protected List<string> split_text(string text)
+    {
+        ///take some form string-text as input
+        ///divides into individual lines, always divided at newlines
+
+        List<string> lines = new List<string>();
+
+        foreach (string line in text.Split('\n'))
+        {
+            if (line != "")
+            {
+                lines.Add(line);
+            }
+            else ///make sure not to have any empty strings
+            {
+                continue;
+            }
+        }
+
+        return lines; ///returns list of strings
+    }
+
+    protected void run_text(List<string> lines, Text text_box, float scroll_speed, bool can_interact) ///default key to interact with dialogue is e
+    {
+        
+    }
+}
