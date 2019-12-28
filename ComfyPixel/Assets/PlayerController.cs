22024313e17a90c32e78f684ae931f0b4d72fc25
Metaxol <45857811+Metaxol@users.noreply.github.com>
2019-12-28 15:43:16 +0100

    Worked on player methods
    - added player_movement method, jumps now based on collisions with ground-tagged objects

---
+++ ComfyPixel/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool onGround = true;
+    //to reactivate jump 
+
+    private void player_movement(float moveSpeed, float jumpHeight)
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        transform.Translate(x * moveSpeed, 0, 0f); //moving method based on transform of player
+
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            //jumping method based on rigidbody-object of player
+
+            onGround = false;
+            //take away ability of jumping
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //reactivating jumping based on collisions with "ground"-tagged objects
+        if (collision.gameObject.tag == "ground") 
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            onGround = false; //to prohibit player from jumping when he fell off an object
+        }
+    }
+
+    private void Update()
+    {
+        player_movement(10f, 10f);
+    }
+}
--- ComfyPixel/Assets/Utility.cs
+++ ComfyPixel/Assets/Utility.cs
@@ -10,12 +10,12 @@ public class Utility : MonoBehaviour {
     private int letter = 0;
     private int current_line = 0;
 
-    int to_change = 0;
+    public int to_change = 0;
 
-    protected List<string> split_text(string text)
+    public List<string> split_text(string text)
     {
-        ///take some form string-text as input
-        ///divides into individual lines, always divided at \n
+        //take some form of string as input
+        //divides into individual lines, always divided at \n
 
         List<string> lines = new List<string>();
 
@@ -25,58 +25,58 @@ public class Utility : MonoBehaviour {
             {
                 lines.Add(line);
             }
-            else ///make sure not to have any empty strings
+            else //make sure not to have any empty strings
             {
                 continue;
             }
         }
 
-        return lines; ///returns list<> of strings
+        return lines; //returns list<> of strings
     }
 
     private IEnumerator scroll_text(float writing_delay, Text text_box, List<string> lines)
     {
         yield return new WaitForSeconds(writing_delay);
-        ///after a certain amount of delay, write a letter
+        //after a certain amount of delay, write a letter
         try
         {
             text_box.text += lines[current_line][letter];
-            ///if an error is produced, give ability to go to next line
+            //if an error is produced, give ability to go to next line
 
             can_scroll = true;
             letter += 1;
         }
         catch
         {
-            ///signal to to give ability to go to next line
+            //signal to to give ability to go to next line
             letter = -1;
         }
     }
 
-    protected void run_text(List<string> lines, Text text_box, float scroll_speed) ///default input is e
+    public void run_text(List<string> lines, Text text_box, float scroll_speed) ///default input is e
     {
         if (can_scroll)
         {
             StartCoroutine(scroll_text(scroll_speed, text_box, lines));
             can_scroll = false;
-            ///introduce bool to prevent repeated execution of coroutine
+            //introduce bool to prevent repeated execution of coroutine
         }
 
         if (letter >= 1)
         {
-            ///ability to complete current line
+            //ability to complete current line
             if (Input.GetKeyDown(KeyCode.E))
             {
                 text_box.text = lines[current_line];
 
                 letter = lines[current_line].Length;
-                ///intentionally producing error to stop further scrolling of current line
+                //intentionally producing error to stop further scrolling of current line
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && letter == -1) ///said signal is being received here
+        if (Input.GetKeyDown(KeyCode.E) && letter == -1) //said signal is being received here
         {
-            ///all values that need to be reset when the next line is going to be shown
+            //all values that need to be reset when the next line is going to be shown
             text_box.text = "";
             current_line += 1;
             letter = 0;
@@ -84,9 +84,9 @@ public class Utility : MonoBehaviour {
         }
     }
 
-    private void options_choosing(int stop, string hor_ver) ///default value for start is 0
+    public void options_choosing(int stop, string hor_ver) ///default value for start is 0
     {
-        ///change value with the right/left arrows
+        //change value with the right/left arrows
         if(hor_ver == "ver")
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -99,7 +99,7 @@ public class Utility : MonoBehaviour {
             }
         }
 
-        ///change value with up/down arrows
+        //change value with up/down arrows
         else if(hor_ver == "hor")
         {
             if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -112,13 +112,13 @@ public class Utility : MonoBehaviour {
             }
         }
 
-        ///if moving method isnt hor or ver, throw an error
+        //if moving method isnt hor or ver, throw an error
         else
         {
             throw new System.Exception("No valid moving method chosen!");
         }
 
-        ///restrict movement of value to certain range
+        //restrict movement of value to certain range
         if(to_change > stop)
         {
             to_change = stop;
@@ -128,9 +128,9 @@ public class Utility : MonoBehaviour {
         }
     }
 
-    private void set_zero()
+    public void set_zero()
     {
-        ///to reset choosing to beginning
+        //to reset choosing to beginning
         to_change = 0; 
     }
 }
