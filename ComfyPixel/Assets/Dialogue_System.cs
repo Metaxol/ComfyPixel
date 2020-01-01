02ecc88846ed8b7746c021c7ae432afb4583d219
Metaxol <45857811+Metaxol@users.noreply.github.com>
2020-01-01 15:55:33 +0100

    restructuring dialogue_system
    - started to add new things to dialogue_system and restructuring it a bit

--- ComfyPixel/Assets/Dialogue_System.cs
+++ ComfyPixel/Assets/Dialogue_System.cs
@@ -7,10 +7,10 @@ public class Dialogue_System : MonoBehaviour {
 
     private Utility utility;
 
-    public void run_dialogue(List<string> lines, Sprite[] sprites, Text TextBox)
+    public void run_dialogue(List<string> lines, Sprite[] sprites, Text TextBox, float scroll_speed, string box_name)
     {
         //just uses utility-class function to start dialogue
-        utility.run_text(lines, TextBox, 0.1f);
+        utility.run_text(lines, TextBox, scroll_speed, box_name);
     }
 
     private void Awake()
--- ComfyPixel/Assets/NPC_Attributes.cs
+++ ComfyPixel/Assets/NPC_Attributes.cs
@@ -6,4 +6,5 @@ public class NPC_Attributes : MonoBehaviour {
     //NPC attributes, more will be added 
     public TextAsset Dialogue;
     public Sprite[] Sprites;
+    public float scroll_speed;
 }
